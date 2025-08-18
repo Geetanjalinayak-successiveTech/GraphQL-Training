a7d770b318e0318ac1b02077a6b2c0b30ecd3bbd
Geetanjali Nayak <geetanjali.nayak@geetanjalissvrs-04.successivetech.com>
2025-08-18 09:45:34 +0530

    feat:Added assignment -2

--- src/modules/message/query.js
+++ src/modules/message/query.js
@@ -1,5 +1,8 @@
 import { messages } from "./dataSource.js";
 
 export const messageQueryResolvers = {
-  messages: () => messages,
+  messages: async () => {
+    await new Promise((resolve) => setTimeout(resolve), 2000);   // Simulate server loading state (2s delay)
+    return messages;
+  },
 };
